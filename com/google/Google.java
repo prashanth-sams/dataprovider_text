32e2350043183d42bd85bac2e895aea56defcb22
prashanth-sams <sams.prashanth@gmail.com>
2014-04-25 16:01:16 +0530

    Changes again

--- com/google/Google.java
+++ com/google/Google.java
@@ -1,19 +1,11 @@
 package com.google;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
-
-import jxl.Cell;
-import jxl.Sheet;
-import jxl.Workbook;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
