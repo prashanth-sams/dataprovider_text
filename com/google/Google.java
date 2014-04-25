0c74bdef35c7bcac2c8d10f9b207063117f4ca6d
Karthikeyan Annamalai <writetokarthikeyan@outlook.com>
2014-04-25 17:00:14 +0530

    added Split logic using StringTokenizer

--- com/google/Google.java
+++ com/google/Google.java
@@ -23,7 +23,7 @@ public class Google {
         
      Object[][] creds = new Object[1][2];
      creds[0][0] = prop.getProperty("search1");
-     creds[0][1] = prop.getProperty("search2");     
+     creds[0][1] = prop.getProperty("search2");
      	// try to print creds by to string here 
 
      return creds;
@@ -38,8 +38,39 @@ public class Google {
 
   
   @Test(dataProvider = "keywords", description = "Google_Test")
-  public void search(String search_keyword1, String search_keyword2) throws Exception {      
-  	// print both search_keyword1 and search_keyword2, i m sure the search_keyword2 will be empty or null
+  public void search(String search_keyword1, String search_keyword2) throws Exception {
+  	
+	
+	
+	
+	 StringTokenizer stKey1 = new StringTokenizer(search_keyword1, ",");
+      StringTokenizer stKey2 = new StringTokenizer(search_keyword2, ",");
+
+      driver.get("http://www.google.co.in");
+
+      while (stKey1.hasMoreElements()) {
+
+          //  search google via keyword 1
+          driver.findElement(By.name("q")).sendKeys();
+          driver.findElement(By.name("q")).submit();
+          Thread.sleep(4000);
+          Boolean a = driver.getTitle().contains("Prashanth Sams"); 
+          System.out.println(a);
+                    
+          while (stKey1.hasMoreElements()) {
+              //  search google via keyword 2      
+              driver.findElement(By.name("q")).clear();
+              driver.findElement(By.name("q")).sendKeys(stKey2.nextElement());
+              Thread.sleep(4000);
+              Boolean b = driver.getTitle().contains("seleniumworks");
+              System.out.println(b);
+      }
+	
+	  
+      }
+	 
+	
+	/*// print both search_keyword1 and search_keyword2, i m sure the search_keyword2 will be empty or null
 
 	  driver.get("http://www.google.co.in");
 	  //  search google via keyword 1
@@ -48,12 +79,12 @@ public class Google {
       Thread.sleep(4000);
       Boolean a = driver.getTitle().contains("Prashanth Sams");
       System.out.println(a);
-	  //  search google via keyword 2      
+	  //  search google via keyword 2
       driver.findElement(By.name("q")).clear();
       driver.findElement(By.name("q")).sendKeys(search_keyword2);
       Thread.sleep(4000);
       Boolean b = driver.getTitle().contains("seleniumworks");
-      System.out.println(b);
+      System.out.println(b);*/
       
   }
 
