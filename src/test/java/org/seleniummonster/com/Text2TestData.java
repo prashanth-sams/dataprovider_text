a3b7c218ba271195b8ac8a2384ca008258723376
Karthikeyan Annamalai <writetokarthikeyan@outlook.com>
2014-04-29 18:52:23 +0530

    Adding Util class

---
+++ src/test/java/org/seleniummonster/com/Text2TestData.java
@@ -0,0 +1,54 @@
+package org.seleniummonster.com;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Text2TestData {
+	
+		private String fileLocation;
+	
+		public Text2TestData(String fileLocation) {
+			super();
+			this.fileLocation = fileLocation;
+		}
+
+	
+	
+	public HashMap<String,String[]> text2TestData(){
+		
+		 FileInputStream fs;
+		 HashMap<String,String[]> keyValuePair=new HashMap<String,String[]>();
+		try {
+			
+			  fs = new FileInputStream(System.getProperty(fileLocation));
+			  DataInputStream in = new DataInputStream(fs);
+			  BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			  
+			  String stringLine;
+			  //Read File Line By Line
+			  while ((stringLine = br.readLine()) != null)   {
+			  // Print the content on the console
+			  System.out.println (stringLine);
+			  String[] keyValue=stringLine.split("=");
+			  keyValuePair.put(keyValue[0],keyValue[1].split(","));
+			  
+			  }
+			  //Close the input stream
+			  in.close();
+			  
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	  
+	     return null;
+		
+		
+	}
+
+}
